71a2dce3016aa84243530750b138b9e92feb4ac3
valentinep <topias.paju@aalto.fi>
2023-11-28 14:38:44 +0200

    finished 1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,20 +1,24 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   const contentArray = [
-    {name: part1, exercises: exercises1},
-    {name: part2, exercises: exercises2},
-    {name: part3, exercises: exercises3}
+    part1, part2, part3
   ]
 
   const totalArray = [
-    exercises1, exercises2, exercises3
+    part1.exercises, part2.exercises, part3.exercises
   ]
 
   const Header = (props) => {

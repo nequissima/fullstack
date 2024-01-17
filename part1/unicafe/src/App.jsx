9047372f3768533a5be8f69c0fbc7f4910c17bef
valentinep <topias.paju@aalto.fi>
2024-01-17 14:47:07 +0200

    making progress on part1

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+const Button = (props) => {
+  <button onClick={props.handleClick}>{props.text}</button>
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      code here
+    </div>
+  )
+}
+
+export default App

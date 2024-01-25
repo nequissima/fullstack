f8ea62ed40bf5ecf3cb6f28ebe1cf56822932d32
valentinep <topias.paju@aalto.fi>
2024-01-25 18:55:20 +0200

    finished 2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+const Number = (person) => {
+  return(<li key={person.name}>{person.name}</li>)
+}
+
+const Numbers = ({persons}) => {
+  return(
+    <ul>
+      {persons.map(person => Number(person))}
+    </ul>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const newpersons = [...persons].concat({name: newName})
+    console.log(newpersons)
+    setPersons(newpersons)
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNote}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Numbers persons={persons}/>
+    </div>
+  )
+
+}
+
+export default App

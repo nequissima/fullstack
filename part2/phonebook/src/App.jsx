ad4d60fb4972093da4555f990651ca212a007f83
valentinep <topias.paju@aalto.fi>
2024-04-17 18:48:00 +0300

    finished 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 const Number = (person) => {
-  return(<li key={person.name}>{person.name}</li>)
+  return(<li key={person.name}>{person.name} {person.number}</li>)
 }
 
 const Numbers = ({persons}) => {
@@ -14,15 +14,19 @@ const Numbers = ({persons}) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addNote = (event) => {
     event.preventDefault()
     
     if (persons.find(number => number.name === newName) === undefined) {
-      const newpersons = [...persons].concat({name: newName})
+      const newpersons = [...persons].concat({name: newName, number: newNumber})
       console.log(newpersons)
       setPersons(newpersons)
     } else {
@@ -36,12 +40,20 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addNote}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange = {handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
